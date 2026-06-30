34b6e5d83169981f9bb0d4b9b4aaa06d0330375f
ad-archer <antonioarcher.dev@gmail.com>
2026-06-30 13:29:52 -0400

    Update:
    add branding and change name to chronogrowth

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-50">
-        Welcome to Chronodesk!
+        Welcome to ChronoGrowth!
       </h1>
       <p className="mt-4 text-lg text-zinc-700 dark:text-zinc-300">
         Create your own time tracker/productivity app for your desk and win a desk/tablet.
